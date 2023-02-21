80228ca700b671e4a37c776908ee73e0501f446c
Deepak <Deepakperiyasamy1212@gmail.com>
2023-02-21 16:45:34 +0530

    Feb-22

--- basic/object/inheritance.js
+++ basic/object/inheritance.js
@@ -1,20 +1,23 @@
-class person1{
-    constructor(name){
-        this.name=name;
-    }
-    display(){
-        console.log(`This is my ${this.name}`);
-    };
- }
- class person2 extends person1{
+// class person1{
+//     constructor(name){
+//         this.name=name;
+//     }
+//     display(){
+//         console.log(`This is my ${this.name}`);
+//     };
+//  }
+//  class person2 extends person1{
 
-    constructor(fname,profession){
-        super(fname);
-        this.profession=profession;
-    }
-    display(){
-        console.log(`This is my ${this.name},${this.profession}`);
-    }
-}
-const people=new person2('Deepak');
-people.display();
+//     constructor(fname,profession){
+//         super(fname);
+//         this.profession=profession;
+//     }
+//     display(){
+//         console.log(`This is my ${this.name},${this.profession}`);
+//     }
+// }
+// const people=new person2('Deepak');
+// // people.display();
+const hello="123";
+//Number(hello);
+console.log(typeof(Number(hello)));
